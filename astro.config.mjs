9399fa69edfc73eb610a2174223be4cd675aafdc
Vahtang <5097094@gmail.com>
2026-07-11 23:32:28 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
+  // Записали домен напрямую для карты сайта
+  site: 'https://elektroschit.com.ua', 
+  integrations: [sitemap()],
 });
